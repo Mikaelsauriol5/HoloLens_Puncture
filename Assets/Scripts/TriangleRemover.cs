f98e4bd7d2744d6dc1f8ceb6da4133086f5ad8d5
buckstarz <kevinchenier1995@gmail.com>
2020-11-05 18:29:56 -0500

    Added script for triangle delete

---
+++ Assets/Scripts/TriangleRemover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleRemover : MonoBehaviour
+{
+    public float sphereRadius;
+    public float maxDistance;
+    public LayerMask layerMask;
+
+    private Vector3 origin;
+    private Vector3 direction;
+
+    private float currentHitDistance;
+
+    void deleteTriangle(int index)
+    {
+        Destroy(gameObject.GetComponent<MeshCollider>());
+        Mesh mesh = transform.GetComponent<MeshFilter>().mesh;
+        int[] oldTriangles = mesh.triangles;
+        int[] newTriangles = new int[mesh.triangles.Length - 3];
+
+        int i = 0;
+        int j = 0;
+
+        while(j < mesh.triangles.Length)
+        {
+            if(j != index * 3)
+            {
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+            }
+            else
+            {
+                j += 3;
+            }
+        }
+        transform.GetComponent<MeshFilter>().mesh.triangles = newTriangles;
+        gameObject.AddComponent<MeshCollider>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        origin = transform.position;
+        direction = transform.forward;
+        RaycastHit hit;
+
+        if(Physics.SphereCast(origin, sphereRadius, direction, out hit, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal))
+            deleteTriangle(hit.triangleIndex);
+    }
+}
